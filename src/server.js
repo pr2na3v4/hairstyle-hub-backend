f9dcdb281126fcbf04126585bc8cbbed14815ad9
pranav kashid <pranavkashid08@gmail.com>
2025-12-27 17:24:29 +0530

    Fix Render port and CORS

--- src/server.js
+++ src/server.js
@@ -50,6 +50,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
+  console.log(`Server running on port ${PORT}`);
 });
