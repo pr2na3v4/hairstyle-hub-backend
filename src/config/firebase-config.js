56ed7d473e37e2da5fd6b553eea3d2faa470d96f
pranav kashid <pranavkashid08@gmail.com>
2025-12-27 16:56:32 +0530

    update auth handling

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,11 +1,16 @@
 import admin from "firebase-admin";
+import dotenv from "dotenv";
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
-);
+dotenv.config(); // MUST be called before accessing process.env
+
+const serviceAccountVar = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+if (!serviceAccountVar) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing in .env file");
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(JSON.parse(serviceAccountVar))
 });
 
 export default admin;
