2c01edf583ec3b9193cc7e7f36144de23c73dd80
pranav kashid <pranavkashid08@gmail.com>
2025-12-27 19:09:53 +0530

    updated server.js

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
