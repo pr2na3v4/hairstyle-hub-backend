c1c513298d4c8639425461168eedb751a34903c8
Pranav Kashid <pranavkashid08@gmail.com>
2026-05-16 22:01:15 +0530

    chore: force clean lockfile

--- src/server.js
+++ src/server.js
@@ -51,5 +51,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
