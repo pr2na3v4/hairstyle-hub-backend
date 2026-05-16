f22fb595601641e7f731e875faf4df373fb81727
Pranav Kashid <pranavkashid08@gmail.com>
2026-05-16 21:11:20 +0530

    tips route fix

--- src/routes/tipRoutes.js
+++ src/routes/tipRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getSmartTips } from '../controllers/tipController.js';
+import { getTips } from '../controllers/tipController.js';
 
 const router = express.Router();
 
 // This endpoint accepts both GET (query parameters) and POST (body payload) data lookups
-router.route('/smart')
-  .get(getSmartTips)
-  .post(getSmartTips);
+router.route('/gettips')
+  .get(getTips)
+  .post(getTips);
 
 export default router;
