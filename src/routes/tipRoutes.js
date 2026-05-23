c56ab26bbad5f1b903f09b95dcc0a7e19ef769ea
Pranav Kashid <pranavkashid08@gmail.com>
2026-05-23 23:29:53 +0530

    tips route fix 4

--- src/routes/tipRoutes.js
+++ src/routes/tipRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // This endpoint accepts both GET (query parameters) and POST (body payload) data lookups
 // NEW MATCHING PATH for the frontend: /api/tips/smart
-router.route('/smart')
+router.route('/gettips')
   .get(getTips)
   .post(getTips);
 
